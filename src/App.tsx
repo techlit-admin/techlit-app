efa2a59615027a96fac1a9a509408c5d673fb5b9
techlit-admin <techlit2025@gmail.com>
2025-05-31 12:36:25 +0530

    Instagram & Portfolio Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Services from './components/Services';
-import Packages from './components/Packages';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import QuickAccessButton from './components/QuickAccessButton';
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Packages from "./components/Packages";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import QuickAccessButton from "./components/QuickAccessButton";
+import Instagram from "./components/Instagram";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-900">
       <Header />
       <Hero />
+      <Instagram />
       <About />
       <Services />
       <Packages />
       <Portfolio />
       <Contact />
       <Footer />
-      <QuickAccessButton/>
+      <QuickAccessButton />
     </div>
   );
 }
---
+++ src/components/Instagram.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Instagram = () => {
+  return (
+    <div className="bg-gray-200 pe-10">
+      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+      <iframe
+        src="//lightwidget.com/widgets/e78f15a993df593a859229b2a24334d2.html"
+        scrolling="no"
+        className="lightwidget-widget mx-6 p-6 text-white"
+        style={{ width: "100%", border: 0 }}
+      ></iframe>
+    </div>
+  );
+};
+
+export default Instagram;
